4470d156cfa3b4266481f462f77a5717c11a175a
2Eos <theserbiandev@gmail.com>
2015-11-30 15:33:03 -0500

    Added query variables for `generateMySQLTables`  to create the mysql tables.

--- src/eos/gir/ebmanagement/api/YamlAPI.java
+++ src/eos/gir/ebmanagement/api/YamlAPI.java
@@ -1,6 +1,7 @@
 package eos.gir.ebmanagement.api;
 
 import eos.gir.ebmanagement.core;
+import org.bukkit.configuration.file.FileConfiguration;
 
 public class YamlAPI {
 
@@ -8,11 +9,13 @@ public class YamlAPI {
 
     private YamlAPI(core plugin){ this.plugin = plugin; }
 
-    public static void grabYAML() {
-
+    public static FileConfiguration grabYAML() {
+        core core = new core();
+        return core.yamlFile.grabYAML();
     }
 
     public static void saveYAML() {
-
+        core core = new core();
+        core.yamlFile.saveYAML();
     }
 }
