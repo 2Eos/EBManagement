a1591bd58e7c6e6b02e98c1ac80ce5ad8a205d9e
2Eos <theserbiandev@gmail.com>
2015-11-30 16:23:19 -0500

    Refactored some code.

--- src/eos/gir/ebmanagement/core.java
+++ src/eos/gir/ebmanagement/core.java
@@ -1,19 +1,19 @@
 package eos.gir.ebmanagement;
 
-import eos.gir.ebmanagement.library.MySQL.Connection;
 import eos.gir.ebmanagement.library.command.CommandFramework;
-import eos.gir.ebmanagement.storage.YamlStorage;
+import eos.gir.ebmanagement.storage.mysql.MySQLStorage;
+import eos.gir.ebmanagement.storage.yaml.YamlStorage;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class core extends JavaPlugin {
 
     public YamlStorage yamlFile;
     public CommandFramework framework;
-    public Connection con;
+    public MySQLStorage con;
 
     public void initialize(){
         yamlFile = new YamlStorage(this);
-        con = new Connection(this);
+        con = new MySQLStorage(this);
     }
 
     public void onEnable() {
