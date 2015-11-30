2b0bdc3abf56d1f9f132527a046d1496428f3f18
2Eos <theserbiandev@gmail.com>
2015-11-30 16:49:23 -0500

    Added MuteListener

---
+++ src/eos/gir/ebmanagement/listeners/MuteListener.java
@@ -0,0 +1,13 @@
+package eos.gir.ebmanagement.listeners;
+
+import eos.gir.ebmanagement.core;
+import org.bukkit.event.Listener;
+
+public class MuteListener implements Listener {
+    private final core plugin;
+
+    public MuteListener(core plugin) {
+        this.plugin = plugin;
+    }
+
+}
