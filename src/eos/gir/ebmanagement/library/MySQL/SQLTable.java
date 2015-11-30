09042be44b49b2f4eaab8965273d6dd164c61f48
Zeljko V <propgamer1@gmail.com>
2015-11-30 09:17:14 -0500

    SQLTable variables to grab queries & table names.

---
+++ src/eos/gir/ebmanagement/library/MySQL/SQLTable.java
@@ -0,0 +1,20 @@
+package eos.gir.ebmanagement.library.MySQL;
+
+/**
+ * Created by zeljko on 11/30/2015.
+ */
+public class SQLTable {
+
+    public static String TABLE_PROFILE = "profile";
+    public static String TABLE_BANS = "bans";
+    public static String TABLE_MUTES = "mutes";
+    public static String TABLE_TEMPBANS = "tempban";
+    public static String TABLE_IPS = "ips";
+
+    public static String TABLE_QUERY_PROFILE = "";
+    public static String TABLE_QUERY_BANS = "";
+    public static String TABLE_QUERY_MUTES = "";
+    public static String TABLE_QUERY_TEMPBANS = "";
+    public static String TABLE_QUERY_IPS = "";
+
+}
