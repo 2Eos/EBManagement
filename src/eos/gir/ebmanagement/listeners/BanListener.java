f247091a6fceda69ca9876e847d022f7e5e8f9ac
2Eos <theserbiandev@gmail.com>
2015-11-30 16:47:15 -0500

    Added a BanListener

---
+++ src/eos/gir/ebmanagement/listeners/BanListener.java
@@ -0,0 +1,12 @@
+package eos.gir.ebmanagement.listeners;
+
+import eos.gir.ebmanagement.core;
+
+public class BanListener {
+    private final core plugin;
+
+    public BanListener(core plugin) {
+        this.plugin = plugin;
+    }
+
+}
