f4558e5590fad4de2c11fcc27c0f557f30b1006f
Zeljko V <propgamer1@gmail.com>
2015-11-30 14:02:01 -0500

    Added query variables for `generateMySQLTables`  to create the mysql tables.

--- src/eos/gir/ebmanagement/library/MySQL/SQLTable.java
+++ src/eos/gir/ebmanagement/library/MySQL/SQLTable.java
@@ -11,10 +11,10 @@ public class SQLTable {
     public static String TABLE_TEMPBANS = "tempban";
     public static String TABLE_IPS = "ips";
 
-    public static String TABLE_QUERY_PROFILE = "";
-    public static String TABLE_QUERY_BANS = "";
-    public static String TABLE_QUERY_MUTES = "";
-    public static String TABLE_QUERY_TEMPBANS = "";
-    public static String TABLE_QUERY_IPS = "";
+    public static String TABLE_QUERY_PROFILE = "CREATE TABLE IF NOT EXISTS `" + TABLE_PROFILE + "` (`UUID` varchar(40),`Name` varchar(32),`IP` varchar(12),`Total Bans` varchar(32),`Total Mutes` varchar(32),`Total Temp Bans` varchar(32), `Total Time Online` varchar(32), `Last Ban Date` varchar(32) )";
+    public static String TABLE_QUERY_BANS = "CREATE TABLE IF NOT EXISTS `" + TABLE_BANS + "` (`UUID` varchar(40),`Name` varchar(32),`Expires in` varchar(50), `Banned by` varchar(32), `Banned on` varchar(32), `Reason` varchar(255), `Unbanned by` varchar(32), `Banned` int(1) ) ";
+    public static String TABLE_QUERY_MUTES = "CREATE TABLE IF NOT EXISTS `" + TABLE_MUTES + "` (`UUID` varchar(40),`Name` varchar(32),`Expires in` varchar(50), `Muted by` varchar(32), `Muted on` varchar(32), `Reason` varchar(255), `Unmuted by` varchar(32), `Muted` int(1) )";
+    public static String TABLE_QUERY_TEMPBANS = "CREATE TABLE IF NOT EXISTS `" + TABLE_TEMPBANS + "` (`UUID` varchar(40),`Name` varchar(32),`Expires in` varchar(50), `Tempbanned by` varchar(32), `Tempbanned on` varchar(32), `Reason` varchar(255), `Unbanned by` varchar(32), `Tempbanned` int(1) )";
+    public static String TABLE_QUERY_IPS = "CREATE TABLE IF NOT EXISTS `" + TABLE_IPS + "` (`UUID` varchar(40),`Name` varchar(32),`IP` varchar(12))";
 
 }
