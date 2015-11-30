d67526becba025d8f7855ae13287c6f7b9a51cb4
2Eos <theserbiandev@gmail.com>
2015-11-30 18:04:35 -0500

    Created Ban Constructor class

---
+++ src/eos/gir/ebmanagement/ban/Ban.java
@@ -0,0 +1,47 @@
+package eos.gir.ebmanagement.ban;
+
+import java.util.UUID;
+
+public class Ban {
+
+    /*
+    Constructor Class, new Ban(UUID, Name)
+    Methods:
+    setPermanent
+    setTime
+     */
+
+    private UUID uuid;
+    private String name;
+    private String reason;
+    private boolean banplayer;
+    private boolean permban;
+    private boolean logged;
+    private int time;
+
+    public Ban(UUID uuid, String name) {
+        this.uuid = uuid;
+        this.name = name;
+    }
+
+    public void setBan(boolean banplayer) {
+        this.banplayer = banplayer;
+    }
+
+    public void setPermanent(boolean permban) {
+        this.permban = permban;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public void setLogging(boolean logged) {
+        this.logged = logged;
+    }
+
+}
