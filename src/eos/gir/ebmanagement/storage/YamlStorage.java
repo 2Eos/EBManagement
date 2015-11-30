18daf9c464206a69812cca9947a4d0bf4f983831
Zeljko V <propgamer1@gmail.com>
2015-11-30 05:37:09 -0500

    Added YamlConfiguration Following Methods createYAML(); grabYAML(); saveYAML():

---
+++ src/eos/gir/ebmanagement/storage/YamlStorage.java
@@ -0,0 +1,60 @@
+package eos.gir.ebmanagement.storage;
+
+import eos.gir.ebmanagement.core;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+
+public class YamlStorage {
+
+    core plugin;
+
+    public File file;
+    public String path;
+    FileConfiguration load;
+
+
+    public YamlStorage(core plugin) {
+        this.plugin = plugin;
+        file = new File(plugin.getDataFolder(), "config.yml");
+        path = plugin.getName() + ".Configuration";
+        load = YamlConfiguration.loadConfiguration(file);
+    }
+
+    public void createYAML(){
+        if ( !file.exists() ) {
+            try {
+                load.set(path + ".Database.Type", "MySQL");
+                load.set(path + ".Database.Host", "");
+                load.set(path + ".Database.Port", "");
+                load.set(path + ".Database.Username", "");
+                load.set(path + ".Database.Password", "");
+                load.set(path + ".Database.Name", "");
+                load.options().copyDefaults(true);
+                load.save(file);
+            }   catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public FileConfiguration grabYAML() {
+        return load;
+    }
+
+    public void saveYAML() {
+        if ( file.exists() ) {
+            try {
+                load.save(file);
+                load.load(file);
+            }   catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            createYAML();
+        }
+    }
+
+
+}
