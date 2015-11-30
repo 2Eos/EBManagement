a8b8eac30370fe57a16abf409251495231788640
2Eos <theserbiandev@gmail.com>
2015-11-30 18:05:45 -0500

    Added createPermBan & createTempBan

--- src/eos/gir/ebmanagement/ban/BanManager.java
+++ src/eos/gir/ebmanagement/ban/BanManager.java
@@ -1,6 +1,26 @@
 package eos.gir.ebmanagement.ban;
 
+import org.bukkit.OfflinePlayer;
+
 public class BanManager {
     private BanManager() {}
+
+    public static void createPermBan(OfflinePlayer player, String reason, boolean logged) {
+        Ban ban = new Ban(player.getUniqueId(), player.getName());
+        ban.setBan(true);
+        ban.setPermanent(true);
+        ban.setLogging(true);
+        ban.setReason(reason);
+    }
+
+    public static void createTempBan(OfflinePlayer player, String name, String reason, int time) {
+        Ban ban = new Ban(player.getUniqueId(), player.getName());
+        ban.setBan(true);
+        ban.setLogging(true);
+        ban.setReason(reason);
+        ban.setTime(time);
+    }
+
+    
 
 }
