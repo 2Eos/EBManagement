c22177b7bbcb0c68c9b2731d0adeecce8739abcd
2Eos <theserbiandev@gmail.com>
2015-11-30 20:28:34 -0500

    Added Profile constructor.

--- src/eos/gir/ebmanagement/profile/Profile.java
+++ src/eos/gir/ebmanagement/profile/Profile.java
@@ -8,6 +8,42 @@ public class Profile {
     private int totalbans;
     private int totalmutes;
     private int totaltempbans;
-    private int lastbandate;
+    private String lastbandate;
+
+    public Profile(String uuid, String name, String ip, int totalbans, int totalmutes, int totaltempbans, String lastbandate) {
+        this.uuid = uuid;
+        this.name = name;
+        this.ip = ip;
+        this.totalbans = totalbans;
+        this.totalmutes = totalmutes;
+        this.totaltempbans = totaltempbans;
+        this.lastbandate = lastbandate;
+    }
+
+    public String getUUID() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTotalBans() {
+        return totalbans;
+    }
+
+    public int getTotalmutes() {
+        return totalmutes;
+    }
+
+    public int getTotalTempBans() {
+        return totaltempbans;
+    }
+
+    private String getLastbandate() {
+        return lastbandate;
+    }
+
+
 
 }
