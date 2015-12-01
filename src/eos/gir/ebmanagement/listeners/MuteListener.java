f3df4eaa66fe74097bc255a8eeace6e1da1fcb43
2Eos <theserbiandev@gmail.com>
2015-11-30 22:04:26 -0500

    Added Registerevents in constructer class!

--- src/eos/gir/ebmanagement/listeners/MuteListener.java
+++ src/eos/gir/ebmanagement/listeners/MuteListener.java
@@ -1,6 +1,7 @@
 package eos.gir.ebmanagement.listeners;
 
 import eos.gir.ebmanagement.core;
+import org.bukkit.Bukkit;
 import org.bukkit.event.Listener;
 
 public class MuteListener implements Listener {
@@ -8,6 +9,7 @@ public class MuteListener implements Listener {
 
     public MuteListener(core plugin) {
         this.plugin = plugin;
+        Bukkit.getPluginManager().registerEvents(this, plugin);
     }
 
 }
