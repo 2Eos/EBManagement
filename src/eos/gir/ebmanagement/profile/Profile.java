d43c3a768b8db6e4ce0f8c1383dc82db8e52339b
2Eos <theserbiandev@gmail.com>
2015-11-30 19:36:52 -0500

    Added Profile comparator

---
+++ src/eos/gir/ebmanagement/profile/Profile.java
@@ -0,0 +1,13 @@
+package eos.gir.ebmanagement.profile;
+
+public class Profile {
+
+    private String uuid;
+    private String name;
+    private String ip;
+    private int totalbans;
+    private int totalmutes;
+    private int totaltempbans;
+    private int lastbandate;
+
+}
